28234865927b384518342df8d40d5cb9087b4dd4
Filipe Falcão <filipefalcaosantos@gmail.com>
2022-07-25 01:35:52 -0300

    Add the __detect_nudity and __detect_violence methods to aws_rekognition.

--- mlaas-fi/services/aws_rekognition.py
+++ mlaas-fi/services/aws_rekognition.py
@@ -4,6 +4,11 @@ from botocore.config import Config
 from .utils import RETRY_TIMES
 
 
+# Default labels for nudity and violence
+NUDITY_LABELS = ['Explicit Nudity']
+VIOLENCE_LABELS = ['Violence', 'Visually Disturbing']
+
+
 class AWSRekognition:
     def __init__(self, aws_config):
         config = Config(retries={'max_attempts': RETRY_TIMES, 'mode': 'standard'})
@@ -23,6 +28,11 @@ class AWSRekognition:
         label_names = [response_label['Name'] for response_label in response_labels]
         return label_names
 
+    # Detects nudity (label 'Explicit Nudity') in an image
+    # Leveraged API: detect_moderation_labels from boto3
+    def __detect_nudity(self, img):
+        return self.__detect_unsafe_labels(img, unsafe_labels=NUDITY_LABELS)
+
     # Detects text occurrences (lines only) in an image
     # Leveraged API: detect_text from boto3
     def __detect_text(self, img):
@@ -33,23 +43,31 @@ class AWSRekognition:
         ]
         return texts_content
 
-    # Detects unsafe content (violence and adult) in an image
+    # Detects unsafe content, defined by the unsafe_labels parameter, in an image
+    # Supported unsafe labels: explicit nudity, suggestive, violence, visually disturbing, rude
+    #                          gestures, drugs, tobacco, alcohol, gambling, hate symbols
     # Leveraged API: detect_moderation_labels from boto3
-    # TODO: handle different label types
-    def __detect_unsafe_labels(self, img):
+    def __detect_unsafe_labels(self, img, unsafe_labels=None):
         response = self.client.detect_moderation_labels(Image=img)
         response_labels = response['ModerationLabels']
-        label_names = [response_label['Name'] for response_label in response_labels]
+        label_names = [
+            resp_label['Name'] for resp_label in response_labels if unsafe_labels is not None and (
+                resp_label['Name'] in unsafe_labels or resp_label['ParentName'] in unsafe_labels
+            )
+        ]
         return label_names
+
+    # Detects violence (labels 'Violence', 'Visually Disturbing') in an image
+    # Leveraged API: detect_moderation_labels from boto3
+    def __detect_violence(self, img):
+        return self.__detect_unsafe_labels(img, unsafe_labels=VIOLENCE_LABELS)
 
     # Recognizes a single celebrity in an image (other celebrities found are not returned)
     # Leveraged API: recognize_celebrities from boto3
     def __recognize_celebrities(self, img):
         response = self.client.recognize_celebrities(Image=img)
         response_celebrities = response['CelebrityFaces']
-        celebrities_ids = [
-            response_celebrity['Name'] for response_celebrity in response_celebrities
-        ]
+        celebrities_ids = [resp_celebrity['Name'] for resp_celebrity in response_celebrities]
         celebrities_ids = celebrities_ids[:1]  # Return only a single celebrity
         return celebrities_ids
 
@@ -59,8 +77,8 @@ class AWSRekognition:
         service_map = {
             'CELEBRITY_RECOGNITION': self.__recognize_celebrities,
             'LABEL_DETECTION': self.__detect_labels,
-            'NUDITY_DETECTION': self.__detect_unsafe_labels,
-            'VIOLENCE_DETECTION': self.__detect_unsafe_labels,
+            'NUDITY_DETECTION': self.__detect_nudity,
+            'VIOLENCE_DETECTION': self.__detect_violence,
             'TEXT_DETECTION': self.__detect_text
         }
 
