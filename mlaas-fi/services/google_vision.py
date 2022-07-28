9eec8c1ae064a6a416d73141deb4cc04ce12ba88
Filipe Falcão <filipefalcaosantos@gmail.com>
2022-07-27 22:49:22 -0300

    Commit the implementation of the Google Vision APIs.

--- mlaas-fi/services/google_vision.py
+++ mlaas-fi/services/google_vision.py
@@ -1,6 +1,11 @@
 from google.cloud import vision
 
 
+# Default labels for nudity and violence
+NUDITY_LABELS = ['adult', 'racy']
+VIOLENCE_LABELS = ['violence']
+
+
 class GoogleVision:
     def __init__(self):
         self.client = vision.ImageAnnotatorClient()
@@ -11,44 +16,59 @@ class GoogleVision:
         image = vision.Image(content=img)
         response = self.client.label_detection(image=image)
         response_labels = response.label_annotations
-        label_names = [response_label['description'] for response_label in response_labels]
+        label_names = [response_label.description for response_label in response_labels]
         return label_names
 
-    # Detects text occurrences (only TEXT_DETECTION type) in an image
+    def __detect_nudity(self, img):
+        return self.__detect_unsafe_labels(img, unsafe_labels=NUDITY_LABELS)
+
+    # Detects text occurrences (only of the TEXT_DETECTION type) in an image
     # Leveraged API: text_detection from google.cloud.vision
     def __detect_text(self, img):
         image = vision.Image(content=img)
         response = self.client.text_detection(image=image)
         response_texts = response.text_annotations
-        print(response_texts)  # TODO: extract detected text
+        texts_content = [resp_text.description for resp_text in response_texts]
+        return texts_content
 
-    # Detects unsafe content (violence and adult) in an image
+    # Detects unsafe content, defined by the unsafe_label parameter, in an image
+    # Supported unsafe labels: adult, medical, spoofed, violence, racy
     # Leveraged API: safe_search_detection from google.cloud.vision
-    def __detect_unsafe_labels(self, img):
+    def __detect_unsafe_labels(self, img, unsafe_labels=None):
+        # Unsafe labels and likelihood names
+        UNSAFE_LABELS = ['adult', 'medical', 'spoofed', 'violence', 'racy']
+        LIKELIHOOD_NAMES = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY',
+                            'VERY_LIKELY')
+
         image = vision.Image(content=img)
         response = self.client.safe_search_detection(image=image)
         response_labels = response.safe_search_annotation
-        print(response_labels)  # TODO: extract detected labels
 
-    def run_service(self, service, images):
-        # Map a service to a function
-        service_fn = None
-        if service == 'LABEL_DETECTION':
-            service_fn = self.__detect_labels
-        elif service == 'NUDITY_DETECTION' or service == 'VIOLENCE_DETECTION':
-            service_fn = self.__detect_unsafe_labels
-        elif service == 'TEXT_DETECTION':
-            service_fn = self.__detect_text
+        # Convert output to the "label-likelihood" format
+        label_names = [
+            # safe_search_annotation isn't subscriptable
+            unsafe_label + '-' + LIKELIHOOD_NAMES[getattr(response_labels, unsafe_label)]
+            for unsafe_label in UNSAFE_LABELS
+            if unsafe_labels is not None and unsafe_label in unsafe_labels
+        ]
 
-        # Apply the function to the given images
-        output_list = []
-        for image in images:
-            with open(image, 'rb') as img_file:
-                img = img_file.read()
-            try:
-                output = service_fn(img)
-                output_list.append(output)
-            except BaseException:
-                print('Unable to run {} for input image {}'.format(service, image))
+        return label_names
 
-        return output_list
+    def __detect_violence(self, img):
+        return self.__detect_unsafe_labels(img, unsafe_labels=VIOLENCE_LABELS)
+
+    # Run an Google Cloud Vision AI service for a given image
+    def run_service(self, service, image):
+        # Map a service to a prediction function
+        service_map = {
+            'LABEL_DETECTION': self.__detect_labels,
+            'NUDITY_DETECTION': self.__detect_nudity,
+            'VIOLENCE_DETECTION': self.__detect_violence,
+            'TEXT_DETECTION': self.__detect_text
+        }
+
+        # Apply the function to the given image
+        with open(image, 'rb') as img_file:
+            img_payload = img_file.read()
+        output = service_map[service](img_payload)
+        return output
